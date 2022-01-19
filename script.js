ef65bbcc7a263417556c8402319b48ab8921ab5f
patrick <patrickodey419@gmail.com>
2022-01-19 10:35:23 +0100

    fixed second lotti file

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const animItem = bodymovin.loadAnimation({
   autoplay: true,
   path: 'https://assets3.lottiefiles.com/packages/lf20_f363x44a.json'
 })
+
+const svgBiker = document.getElementById('bike-svg');
+const animItem2 = bodymovin.loadAnimation({
+  wrapper: svgBiker,
+  animType: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets5.lottiefiles.com/packages/lf20_azp4pamb.json'
+});
+
+
+
+
+
